87b7b45aa4e60b2ebe52431e4c231db24bf32aa2
Roman <leshchukr2004@gmail.com>
2024-01-09 23:13:56 +0300

    Add weight system.
    Took 44 minutes

--- src/main/java/xyz/sherhsnyaga/bettercallfishing/config/LangConfig.java
+++ src/main/java/xyz/sherhsnyaga/bettercallfishing/config/LangConfig.java
@@ -1,9 +1,11 @@
 package xyz.sherhsnyaga.bettercallfishing.config;
 
+import lombok.Getter;
 import org.bukkit.configuration.file.YamlConfiguration;
 
+@Getter
 public class LangConfig {
-    private String massUnit;
+    private String getWeightString;
     public void load(YamlConfiguration config) {
 
     }
---
+++ src/main/java/xyz/sherhsnyaga/bettercallfishing/config/WeightConfig.java
@@ -0,0 +1,80 @@
+package xyz.sherhsnyaga.bettercallfishing.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class WeightConfig {
+    public static final NamespacedKey FISH_WEIGHT_NAMESPACE = NamespacedKey.fromString("fish_weight");
+    private static final List<String> FISH_TYPES = List.of(
+            "COD",
+            "SALMON",
+            "PUFFERFISH",
+            "TROPICAL_FISH"
+    );
+    private final Random random;
+    private final MiniMessage miniMessage;
+
+    @Getter
+    private boolean isWeightEnabled;
+    private HashMap<Material, FishSettings> fishSettings;
+    private LangConfig langConfig;
+
+    public WeightConfig(FileConfiguration config, LangConfig langConfig) {
+        fishSettings = new HashMap<>();
+        random = new Random();
+        miniMessage = MiniMessage.builder().build();
+        setConfiguration(config);
+    }
+
+    public void setConfiguration(FileConfiguration config) {
+        isWeightEnabled = config.getBoolean("weight.enable");
+
+        for (String type: FISH_TYPES) {
+            double minWeight = config.getDouble("weight." + type + ".min");
+            double maxWeight = config.getDouble("weight." + type + ".max");
+            fishSettings.put(Material.matchMaterial(type), new FishSettings(minWeight, maxWeight));
+        }
+    }
+
+    public ItemStack genWeight(ItemStack itemStack) {
+        if (itemStack.getAmount() == 1) {
+            if (!fishSettings.containsKey(itemStack.getType()))
+                return itemStack;
+
+            FishSettings settings = fishSettings.get(itemStack.getType());
+
+            double weight = getRandom(settings.minWeight, settings.maxWeight);
+            String formattedWeight = String.format("%.3f", weight);
+
+            String nameStr = langConfig.getGetWeightString();
+            nameStr = nameStr.replace("%weight%", formattedWeight);
+            ItemMeta meta = itemStack.getItemMeta();
+            meta.lore(List.of(miniMessage.deserialize(nameStr)));
+            meta.getPersistentDataContainer().set(FISH_WEIGHT_NAMESPACE, PersistentDataType.DOUBLE, weight);
+            itemStack.setItemMeta(meta);
+        }
+
+        return itemStack;
+    }
+
+    private double getRandom(double min, double max) {
+        return min + (max - min) * random.nextDouble();
+    }
+
+    @AllArgsConstructor
+    private class FishSettings {
+        private double minWeight;
+        private double maxWeight;
+    }
+}
