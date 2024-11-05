f73bd2cea87a2e340aadec4735a7543b8aa34044
Shershnyaga <leshchukr2004@gmail.com>
2024-11-05 14:37:06 +0300

    YamlSerializable config on EnchantmentSerializer

--- src/main/java/me/shershnyaga/bettercallfishing/utils/serializers/EnchantmentSerializer.java
+++ src/main/java/me/shershnyaga/bettercallfishing/utils/serializers/EnchantmentSerializer.java
@@ -1,15 +1,23 @@
 package me.shershnyaga.bettercallfishing.utils.serializers;
 
 import lombok.Getter;
+import net.elytrium.serializer.SerializerConfig;
 import net.elytrium.serializer.language.object.YamlSerializable;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.meta.ItemMeta;
 
 @Getter
 public class EnchantmentSerializer extends YamlSerializable {
+
+    private static final SerializerConfig CONFIG = new SerializerConfig.Builder().build();
+
     private String enchantment;
     private int lvl;
     private boolean hide;
+
+    public EnchantmentSerializer() {
+        super(CONFIG);
+    }
 
     public void append(ItemMeta meta) {
         Enchantment enchantment = Enchantment.getByName(this.enchantment);
