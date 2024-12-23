8bfa873336d01578e8339ea59bbadb7c1b540083
Shershnyaga <leshchukr2004@gmail.com>
2024-12-23 20:16:24 +0300

    MythicMobs config implementation

--- src/main/java/me/shershnyaga/bettercallfishing/config/MythicMobsConfig.java
+++ src/main/java/me/shershnyaga/bettercallfishing/config/MythicMobsConfig.java
@@ -1,11 +1,56 @@
 package me.shershnyaga.bettercallfishing.config;
 
+import io.lumine.mythic.api.mobs.MythicMob;
+import io.lumine.mythic.bukkit.BukkitAdapter;
+import io.lumine.mythic.bukkit.MythicBukkit;
+import io.lumine.mythic.core.mobs.ActiveMob;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import me.shershnyaga.bettercallfishing.utils.integrations.MythicMobsUtil;
+import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
 
+import java.util.HashMap;
+import java.util.Optional;
+
+@Getter
 public class MythicMobsConfig {
+
+    private HashMap<String, MythicMobInfo> mobs = new HashMap<>();
 
     public MythicMobsConfig(FileConfiguration config) {
 
+        config.getKeys(false).forEach(key -> {
+
+            float chance = (float) config.getDouble(key + ".catch-chance");
+
+            mobs.put(key, new MythicMobInfo(key, chance));
+        });
+
     }
 
+
+    @AllArgsConstructor
+    @Getter
+    public static class MythicMobInfo {
+        private String id;
+        private float spawnChance;
+
+        public Optional<Entity> spawn(Location spawnLocation) {
+
+            if (!MythicMobsUtil.isEnabled()) {
+                return Optional.empty();
+            }
+
+            MythicMob mob = MythicBukkit.inst().getMobManager().getMythicMob(id).orElse(null);
+
+            if(mob == null) {
+                return Optional.empty();
+            }
+
+            ActiveMob activeMob = mob.spawn(BukkitAdapter.adapt(spawnLocation),1);
+            return Optional.of(activeMob.getEntity().getBukkitEntity());
+        }
+    }
 }
