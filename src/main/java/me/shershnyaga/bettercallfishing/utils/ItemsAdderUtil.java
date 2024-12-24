296eae34959bd31ad1f63e8ffc6faf0fe096ffdf
Shershnyaga <leshchukr2004@gmail.com>
2024-12-25 01:17:26 +0300

    Remove old ItemsAdderUtil

--- src/main/java/me/shershnyaga/bettercallfishing/utils/ItemsAdderUtil.java
+++
@@ -1,9 +0,0 @@
-package me.shershnyaga.bettercallfishing.utils;
-
-import org.bukkit.Bukkit;
-
-public final class ItemsAdderUtil {
-    public static boolean isEnabled() {
-        return Bukkit.getPluginManager().getPlugin("ItemsAdder") != null;
-    }
-}
