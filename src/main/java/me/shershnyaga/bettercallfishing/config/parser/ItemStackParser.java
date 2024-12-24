1a844b8682d870782f91386c53a1843e095c8e7e
Shershnyaga <leshchukr2004@gmail.com>
2024-12-24 16:30:29 +0300

    ItemStack parser example

--- src/main/java/me/shershnyaga/bettercallfishing/config/parser/ItemStackParser.java
+++ src/main/java/me/shershnyaga/bettercallfishing/config/parser/ItemStackParser.java
@@ -1,5 +1,32 @@
 package me.shershnyaga.bettercallfishing.config.parser;
 
-public class ItemStackParser {
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
+
+public final class ItemStackParser {
+
+    public static ParsedItem parse(ConfigurationSection section) {
+        String materialName = section.getString("material");
+        List<String> lore = section.getStringList("lore");
+        int cmd = section.getInt("custom-model-data");
+    }
+
+
+    public static class ParsedItem {
+        public ItemStack toItemStack() {
+            return toItemStack(1);
+        }
+
+        public ItemStack toItemStack(int count) {
+            return null;
+        }
+
+        public ConfigurationSection dump() {
+            return null;
+        }
+    }
 
 }
