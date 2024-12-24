1329c317339e60dd7eb34d3641996d5f2acc6c8c
Shershnyaga <leshchukr2004@gmail.com>
2024-12-24 17:07:53 +0300

    Added caught_items config

--- src/main/java/me/shershnyaga/bettercallfishing/config/parser/ItemStackParser.java
+++ src/main/java/me/shershnyaga/bettercallfishing/config/parser/ItemStackParser.java
@@ -1,27 +1,46 @@
 package me.shershnyaga.bettercallfishing.config.parser;
 
+import lombok.AllArgsConstructor;
+import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
 
 public final class ItemStackParser {
 
+    private static final String MATERIAL_SECTION = "material";
+    private static final String LORE_SECTION = "lore";
+    private static final String CMD_SECTION = "cmd";
+
     public static ParsedItem parse(ConfigurationSection section) {
+
+        String materialName = section.getString(MATERIAL_SECTION);
+        if (materialName.contains("IA:")) {
+
+        }
+
+        ItemStack parsedItem = new ItemStack(Material.AIR);
+
         String materialName = section.getString("material");
         List<String> lore = section.getStringList("lore");
         int cmd = section.getInt("custom-model-data");
     }
 
 
+    @AllArgsConstructor
     public static class ParsedItem {
+
+        private final ItemStack item;
+
         public ItemStack toItemStack() {
             return toItemStack(1);
         }
 
         public ItemStack toItemStack(int count) {
-            return null;
+            ItemStack i = item.clone();
+            i.setAmount(count);
+            return i;
         }
 
         public ConfigurationSection dump() {
