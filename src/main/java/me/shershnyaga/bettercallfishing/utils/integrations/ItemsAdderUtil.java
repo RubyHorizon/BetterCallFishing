f6c64e1f0dd81d2da718aa80ab8cfc5832d64512
Shershnyaga <leshchukr2004@gmail.com>
2024-12-25 02:52:54 +0300

    ItemsAdderUtil and MMOItems util changes

--- src/main/java/me/shershnyaga/bettercallfishing/utils/integrations/ItemsAdderUtil.java
+++ src/main/java/me/shershnyaga/bettercallfishing/utils/integrations/ItemsAdderUtil.java
@@ -1,9 +1,37 @@
 package me.shershnyaga.bettercallfishing.utils.integrations;
 
+import dev.lone.itemsadder.api.CustomStack;
+import me.shershnyaga.bettercallfishing.BetterCallFishing;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Optional;
 
 public final class ItemsAdderUtil {
     public static boolean isEnabled() {
         return Bukkit.getPluginManager().getPlugin("ItemsAdder") != null;
+    }
+
+    public static Optional<ItemStack> getIAItem(String id) {
+        if (!isEnabled()) {
+            BetterCallFishing.log(ChatColor.RED + "\""
+                    + id + "\" this is an ItemsAdder item, but the ItemsAdder plugin " +
+                    "is not loaded!");
+            return Optional.empty();
+        }
+
+        String iaId = id.replace("IA:", "");
+        if (CustomStack.isInRegistry(iaId)) {
+
+            ItemStack item = CustomStack.getInstance(iaId).getItemStack().clone();
+
+            return Optional.of(item);
+        }
+
+        BetterCallFishing.log(ChatColor.RED + "\""
+                + id + "\" is not registered in ItemsAdder!");
+
+        return Optional.empty();
     }
 }
--- src/main/java/me/shershnyaga/bettercallfishing/utils/integrations/MMOItemsUtil.java
+++ src/main/java/me/shershnyaga/bettercallfishing/utils/integrations/MMOItemsUtil.java
@@ -1,9 +1,11 @@
 package me.shershnyaga.bettercallfishing.utils.integrations;
 
+import me.shershnyaga.bettercallfishing.BetterCallFishing;
 import net.Indyuce.mmoitems.MMOItems;
 import net.Indyuce.mmoitems.api.item.mmoitem.MMOItem;
 import net.Indyuce.mmoitems.api.Type;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Optional;
@@ -16,15 +18,30 @@ public final class MMOItemsUtil {
     public Optional<ItemStack> getItem(String type, String id) {
 
         if (!isEnabled()) {
+            BetterCallFishing.log(ChatColor.RED + "\""
+                    + id + "\" this is an MMOItems item, but the MMOItems plugin " +
+                    "is not loaded!");
             return Optional.empty();
         }
 
         MMOItem mmoitem = MMOItems.plugin.getMMOItem(Type.get(type), id);
 
         if (mmoitem != null) {
-            return Optional.ofNullable(mmoitem.newBuilder().build());
+
+            ItemStack item = mmoitem.newBuilder().build();
+
+            if (item != null) {
+                return Optional.of(item);
+            } else {
+                BetterCallFishing.log(ChatColor.RED + "\""
+                        + id + "\" is not registered in MMOItems!");
+            }
+
         } else {
-            return Optional.empty();
+            BetterCallFishing.log(ChatColor.RED + "\""
+                    + id + "\" is not registered in MMOItems!");
         }
+
+        return Optional.empty();
     }
 }
