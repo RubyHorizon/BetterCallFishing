1a5e1401c328fcda842e918b094d9329acc51d7d
Shershnyaga <leshchukr2004@gmail.com>
2024-12-27 22:25:38 +0300

    Removed MythicMobsUtil

--- src/main/java/me/shershnyaga/bettercallfishing/utils/integrations/MythicMobsUtil.java
+++
@@ -1,9 +0,0 @@
-package me.shershnyaga.bettercallfishing.utils.integrations;
-
-import org.bukkit.Bukkit;
-
-public final class MythicMobsUtil {
-    public static boolean isEnabled() {
-        return Bukkit.getPluginManager().getPlugin("MythicMobs") != null;
-    }
-}
